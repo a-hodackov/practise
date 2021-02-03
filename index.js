1ee945f72e6ab051de8f5cce06670269b8a9fcf4
a-hodackov <hodackoff1995@gmail.com>
2021-02-04 02:33:57 +0300

    add practise chapter 2

--- index.js
+++ index.js
@@ -9,10 +9,30 @@ const personalMovieDB = {
   private: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов");
-const b = prompt("Оценка фильма");
-const c = prompt("Один из последних просмотренных фильмов");
-const d = prompt("Оценка фильма");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const a = prompt("Один из последних просмотренных фильмов");
+// const b = prompt("Оценка фильма");
+// const c = prompt("Один из последних просмотренных фильмов");
+// const d = prompt("Оценка фильма");
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов");
+  const b = prompt("Оценка фильма");
+  if (a != null && b != null && a != "" && b != "" && a.length != 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
 console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  alert("Вы киноман");
+} else alert("Ошибка");
+//console.log(personalMovieDB);
